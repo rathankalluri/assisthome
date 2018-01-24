00ae40eb13dc0a79b86a39b5a498126fef7eb751
rathankalluri <ratan.kalluri@gmail.com>
2018-01-24 13:54:32 +0530

    Update action.php

--- action.php
+++ action.php
@@ -7,10 +7,12 @@ function sendMessage($parameters)
 }
 $post      = file_get_contents("php://input");
 $post_data = json_decode($post, True);
+error_log($post, 0);
 
 if (isset($post_data["result"]["action"])) {
     $url        = "http://rathankalluri.com/lab/fetch.php?getdetails";
     $speak      = file_get_contents($url);
+    error_log($speak, 0);
     $data = json_decode($speak, true);
     //$speak      = substr($speak, 0, strpos($speak, "(c)"));  
 }     
